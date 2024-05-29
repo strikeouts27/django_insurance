d265e001c2786a00f629b44cc56a0b7d0e14b851
Andrew Stribling <stribling.andrew@gmail.com>
2024-05-28 20:52:15 -0500

    reconciled urls.py

--- django_insurance/django_insurance/urls.py
+++ django_insurance/django_insurance/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_insurance project.
+URL configuration for web_scrap_cloud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# urls.py -> django_project url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("quote/", include("quote.urls")),
 ]
