829092ed732d0753f17269f00f9030426509c297
Andrew Stribling <stribling.andrew@gmail.com>
2024-05-28 21:13:08 -0500

    Safestring Error

--- django_insurance/django_insurance/settings.py
+++ django_insurance/django_insurance/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'dfwpythoneers'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
