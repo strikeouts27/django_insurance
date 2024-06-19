2ec99cc37bff17105edf066031a373142d83e3bb
Andrew Stribling <stribling.andrew@gmail.com>
2024-06-18 21:20:22 -0500

    the label attribute is used to label form fields which allow us to write the questions we want for our form

--- django_insurance/quote/forms.py
+++ django_insurance/quote/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class Customer_Form(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
+    address = forms.CharField(max_length=20, label="What is your address?")
