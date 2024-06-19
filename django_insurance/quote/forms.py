9c0425fa81519c4a5a98d3900f37093e23cc09a5
Andrew Stribling <stribling.andrew@gmail.com>
2024-06-18 22:18:13 -0500

    customer form basics completed with labels.

--- django_insurance/quote/forms.py
+++ django_insurance/quote/forms.py
@@ -1,6 +1,16 @@
 from django import forms 
 
 class Customer_Form(forms.Form):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    address = forms.CharField(max_length=20, label="What is your address?")
+    first_name = forms.CharField(max_length=20, label="What is your first name?", required=True)
+    last_name = forms.CharField(max_length=20, label='What is your last name?', required=True)
+    address = forms.CharField(max_length=20, label="What is your address?", required=True)
+    zip_code = forms.IntegerField(label="What is your zip code?")
+    # dashes between the number 
+    telephone_number = forms.IntegerField(required=True, label='What is your phone number?')
+    email = forms.EmailField(max_length= 20, required=False, label='What is your email address?')
+    date_of_birth = forms.DateField(required=True, label='What is your date of birth?')
+
+
+
+
+
