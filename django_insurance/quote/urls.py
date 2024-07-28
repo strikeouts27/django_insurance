18cda1db64caca309f659133fd91c15fadd69195
eddyizm <eddyizm@gmail.com>
2024-07-28 12:53:44 -0700

    feature: flow-changes-css-framework-quote-id

--- django_insurance/quote/urls.py
+++ django_insurance/quote/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from quote.views import AboutPageView, Customer_CreateView, Customer_UpdateView, HomePageView, driver_form
+from quote.views import (
+    AboutPageView,
+    Customer_CreateView,
+    HomePageView,
+    driver_form
+    # Customer_UpdateView,
+)
 
 
 urlpatterns = [
     path("quote/customer/create/", Customer_CreateView.as_view(), name='customer'),
-    path("quote/customer/<int:pk>", Customer_UpdateView.as_view(),),
+    # path("quote/customer/<int:pk>", Customer_UpdateView.as_view(),),
     path("", HomePageView.as_view(), name='home'),
     path("about/", AboutPageView.as_view(), name="about"),
     path("driver/", driver_form, name="driver"),
